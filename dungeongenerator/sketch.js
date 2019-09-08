59827ec62efb1243eb379b531a3edde5a0b222c3
BuildTools <unconfigured@null.spigotmc.org>
2019-09-07 22:05:55 -0600

    Renamed MazeGen to DungeonGen. Gen is done, just need to add GUI.

---
+++ dungeongenerator/sketch.js
@@ -0,0 +1,582 @@
+// Birbs/Boids
+// Timothy Letkeman
+// Wednesday 5th September, 2019
+//
+// Extra for Experts:
+// - more complicated than expected of cs30 because it deals with classes and whatnot
+// - Gui for editing rules
+// - decently complicated to implement all of the rules
+
+const Passage = {
+	Unused: 0,
+	Open: 1,
+	Blocked: 2
+};
+
+const Directions = {
+	North: 0,
+	East: 1,
+	South: 2,
+	West: 3
+};
+
+class MazeCell 
+{
+	constructor(x, y) 
+	{
+		this.x = x;
+		this.y = y;
+
+		this.region = -1;
+		this.walled = true;
+
+		this.r = closedBrightness;
+		this.g = closedBrightness;
+		this.b = closedBrightness;
+	}
+
+	render()
+	{
+		if (this.r === 0 && this.g === 0 && this.b === 0)
+		{
+			return;
+		}
+		if (cellOutline)
+		{
+			stroke(this.r - 130, this.g - 130, this.b - 130);
+		}
+		fill(this.r, this.g, this.b);
+		rect(this.x * pixelsPerCell, this.y * pixelsPerCell, pixelsPerCell, pixelsPerCell)
+	}
+
+	setOpen()
+	{
+		this.r = openBrightness;
+		this.g = openBrightness;
+		this.b = openBrightness;
+
+		this.walled = false;
+	}
+
+	setClosed()
+	{
+		this.r = closedBrightness;
+		this.g = closedBrightness;
+		this.b = closedBrightness;
+
+		this.walled = true;
+	}
+
+	changeColour(newR, newG, newB)
+	{
+		this.r = newR;
+		this.g = newG;
+		this.b = newB;
+	}
+}
+
+class VertexMazeCell extends MazeCell
+{
+	constructor(x, y) 
+	{
+		super(x, y);
+		this.allCells = null;
+		this.isStart = false;
+
+		this.neighbors = null;
+		this.initializedEdgeCount = 0;
+		this.passages = new Array(4);
+	}
+
+	setup(neighbors, allCells)
+	{
+		this.neighbors = neighbors;
+		this.allCells = allCells;
+		for (let i = 0; i < 4; i++)
+		{
+			this.passages[i] = Passage.Unused;
+		}
+	}
+
+	setStart()
+	{
+		this.isStart = true;
+		this.changeColour(0,255,0);
+	}
+
+	awake(region)
+	{
+		this.region = region;
+		this.walled = false;
+		this.setOpen();
+	}
+
+	setPassage(direction, type)
+	{
+		this.setSoloPassage(direction, type);
+		if (type == Passage.Open)
+		{
+			this.allCells[this.x + VertexMazeCell.getDirectionX(direction)][this.y + VertexMazeCell.getDirectionY(direction)].setOpen();
+			this.allCells[this.x + VertexMazeCell.getDirectionX(direction)][this.y + VertexMazeCell.getDirectionY(direction)].region = this.region;
+		}
+		if (this.neighbors[direction] !== null)
+		{
+			this.neighbors[direction].setSoloPassage((direction + 2) % 4, type);
+		}
+	}
+
+	get getAvailableDirection()
+	{
+		let skips = floor(random(0, 4 - this.initializedEdgeCount));
+		for (let i = 0; i < 4; i++)
+		{
+			if (this.passages[i] == Passage.Unused)
+			{
+				if (skips == 0)
+				{
+					return i;
+				}
+				skips -= 1;
+			}
+		}
+		return NaN;
+	}
+
+	setSoloPassage(direction, type)
+	{
+		this.initializedEdgeCount++;
+		this.passages[direction] = type;
+	}
+
+	static getDirectionX(direction)
+	{			// East			 	// West
+		return direction == 1 ? 1 : direction == 3 ? -1 : 0
+	}
+
+	static getDirectionY(direction)
+	{			// North			 // South
+		return direction == 0 ? 1 : direction == 2 ? -1 : 0
+	}
+}
+
+const openBrightness = 255;
+
+const closedBrightness = 0;
+
+//preset values
+let roomAttempts = 80;
+let roomMaxSize = 3;
+let roomMinSize = 1;
+
+let deadEndRemoval = 0.85;
+let pixelsPerCell = 16;
+let cellOutline = true;
+
+let cells;
+
+let cols;
+let vertexCols;
+let rows;
+let vertexRows;
+
+let activeCells;
+let curRegion;
+let startCell;
+let rooms;
+
+function get2dArray(cols, rows)
+{
+	//returns an empty two dimensional array with the given rows and columns
+	let returnable = new Array(cols);
+    for (let i = 0; i < cols; i++)
+  	{
+    	returnable[i] = new Array(rows);
+  	}
+ 	return returnable;
+}
+
+function setup() 
+{
+	//caps frames at 60
+	frameRate(100);
+	//Basic setup of starting flight area 
+	createCanvas(windowWidth, windowHeight);
+
+	reset();
+}
+
+function reset()
+{
+	cols = Math.floor((windowWidth / pixelsPerCell) / 2) * 2;
+	if (cols + 1 > windowWidth / pixelsPerCell)
+	{
+		cols--;
+	}
+	else
+	{
+		cols++;
+	}
+	vertexCols = floor((cols - 1) / 2);
+
+	rows = Math.floor((windowHeight / pixelsPerCell) / 2) * 2;
+	if (rows + 1 > windowHeight / pixelsPerCell)
+	{
+		rows--;
+	}
+	else
+	{
+		rows++;
+	}
+	vertexRows = floor((rows - 1) / 2);
+
+	cells = get2dArray(cols, rows);
+	for (let x = 0; x < cols; x++)
+	{
+		for (let y = 0; y < rows; y++)
+		{
+			if (x % 2 == 1 && y % 2 == 1)
+			{
+				cells[x][y] = new VertexMazeCell(x, y);
+			}
+			else
+			{
+				cells[x][y] = new MazeCell(x, y);
+			}
+		}
+	}
+
+	for (let x = 0; x < cols; x++)
+	{
+		for (let y = 0; y < rows; y++)
+		{
+			if (x % 2 == 1 && y % 2 == 1)
+			{
+				cells[x][y].setup([getCell(x, y + 2), getCell(x + 2, y), getCell(x, y - 2), getCell(x - 2, y)], cells);
+			}
+		}
+	}
+
+	generate();
+}
+
+function generate()
+{
+	curRegion = 0;
+
+	generateRooms();
+	generateMaze();
+	connectRegions();
+	removeDeadEnds();
+	setEnd();
+}
+
+function generateRooms()
+{
+	rooms = new Array();
+	for (let i = 0; i < roomAttempts; i++)
+	{
+		let xSize = floor(random(roomMinSize, roomMaxSize + 1));
+		let ySize = floor(random(roomMinSize, roomMaxSize + 1));
+
+		let xPos = floor(random(0, vertexCols - xSize));
+		let yPos = floor(random(0, vertexRows - ySize));
+
+		if (xPos + xSize < vertexCols && yPos + ySize < vertexRows)
+		{
+			let succeded = true;
+			for (let x = xPos; x < xPos + xSize + 1; x++)
+			{
+				for (let y = yPos; y < yPos + ySize + 1; y++)
+				{
+					if (!cells[x * 2 + 1][y * 2 + 1].walled)
+					{
+						succeded = false;
+						break;
+					}
+				}
+				if (!succeded)
+				{
+					break;
+				}
+			}
+			if (succeded)
+			{
+				let colour = floor(random(0,3));
+				let r;
+				let g;
+				let b;
+				
+				if (colour == 0)
+				{
+					r = 255 - floor(random(0, 50));
+					g = 255 - floor(random(30, 140));
+					b = 255 - floor(random(30, 140));
+				}
+				else if (colour == 1)
+				{
+					r = 255 - floor(random(30, 140));
+					g = 255 - floor(random(0, 50));
+					b = 255 - floor(random(30, 140));
+				}
+				else
+				{
+					r = 255 - floor(random(30, 140));
+					g = 255 - floor(random(30, 140));
+					b = 255 - floor(random(0, 50));
+				}
+
+				for (let x = xPos * 2 + 1; x < (xPos + xSize) * 2 + 2; x++)
+				{
+					for (let y = yPos * 2 + 1; y < (yPos + ySize) * 2 + 2; y++)
+					{
+						cells[x][y].walled = false;
+						cells[x][y].changeColour(r,g,b);
+						cells[x][y].region = curRegion;
+					}
+				}
+				rooms.push([xPos, yPos, xSize, ySize]);
+				curRegion++;
+			}
+		}
+	}
+}
+
+function generateMaze()
+{
+	let pos = getValidPosition();
+	while (pos !== null)
+	{
+		activeCells = new Array();
+		activeCells.push(cells[pos[0]][pos[1]]);
+		activeCells[0].awake(curRegion);
+
+		while (activeCells.length > 0)
+		{
+			doMazeGen();
+		}
+		curRegion++;
+		pos = getValidPosition();
+	}
+}
+
+function connectRegions()
+{
+	let regions = new Array(curRegion);
+	for (let i = 0; i < curRegion; i++)
+	{
+		regions[i] = new Array();
+	}
+
+	for (let x = 1; x < cols - 1; x++)
+	{
+		for (let y = 1; y < rows - 1; y++)
+		{
+			for (let d = 0; d < 4; d++) 
+			{
+				//Dont go off the edge looking for connections	
+				if ((y == 1 || y == 2) && d == 2)
+				{
+					continue;
+				} 
+				if ((x == 1 || x == 2) && d == 3)
+				{
+					continue;
+				} 
+				if ((y == rows - 2 || y == rows - 3) && d == 0)
+				{
+					continue;
+				} 
+				if ((x == cols - 2 || x == cols - 3) && d == 1)
+				{
+					continue;
+				} 
+
+				let cellOne = cells[x][y];
+				let cellTwo = cells[x + VertexMazeCell.getDirectionX(d) * 2][y + VertexMazeCell.getDirectionY(d) * 2];
+
+				if (cellOne.region !== -1 && cellTwo.region !== -1 && cellOne.region !== cellTwo.region)
+				{
+					regions[cellOne.region].push([cellTwo.region, x + VertexMazeCell.getDirectionX(d), y + VertexMazeCell.getDirectionY(d)]);
+				}
+			}
+		}
+	}
+	//[0] == otherRegion, [1] == x, [2] == y
+	let mergedRegions = new Array(0);
+	mergedRegions.push(0);
+	while (regions[0].length > 0)
+	{
+		let connection = regions[0][floor(random(0, regions[0].length))];
+		cells[connection[1]][connection[2]].setOpen();
+
+		while (regions[connection[0]].length > 0)
+		{
+			regions[0].push(regions[connection[0]].pop());
+		}
+
+		mergedRegions.push(connection[0]);
+		for (let j = 0; j < regions[0].length; j++)
+		{
+			if (mergedRegions.includes(regions[0][j][0]))
+			{
+				regions[0].splice(j, 1);
+				j--;
+			}
+		}
+	}
+}
+
+function removeDeadEnds()
+{
+	let deadEnds = new Array();
+	for (let x = 0; x < vertexCols; x++)
+	{
+		for (let y = 0; y < vertexRows; y++)
+		{
+			let dir = isDeadEnd([x * 2 + 1, y * 2 + 1]);
+			if (dir !== null)
+			{
+				deadEnds.push([dir, x * 2 + 1, y * 2 + 1]);
+			}
+		}
+	}
+
+	//set a dead end as the start
+	let index = floor(random(0, deadEnds.length));
+	startCell = cells[deadEnds[index][1]][deadEnds[index][2]];
+	deadEnds.splice(index, 1);
+	startCell.setStart();
+
+	//kill deadends
+	for (let i = 0; i < deadEnds.length; i++)
+	{
+		if (random(1) < deadEndRemoval)
+		{
+			let pos = [deadEnds[i][1], deadEnds[i][2]];
+			let dir = deadEnds[i][0];
+			while (dir !== null)
+			{
+				cells[pos[0]][pos[1]].setClosed();
+				pos[0] = pos[0] + VertexMazeCell.getDirectionX(dir);
+				pos[1] = pos[1] + VertexMazeCell.getDirectionY(dir);
+				dir = isDeadEnd(pos);
+			}
+		}
+	}
+}
+
+function setEnd()
+{
+	//picks farthest room (with randomness)
+
+	let heighestDistance = -5318008; // heh boobies
+	let heighestRoomIndex = -1;
+	for (let i = 0; i < rooms.length; i++) //0 == xPos, 1 == yPos, 2 == xSize, 3 == ySize
+	{
+		let distance = (Math.abs(startCell.x - rooms[i][0] * 2 + 1 + rooms[i][2] / 2) +  Math.abs(startCell.y - rooms[i][1] * 2 + 1 + rooms[i][3] / 2)) * random(0.5, 1);
+		print(distance);
+		if (distance > heighestDistance)
+		{
+			heighestDistance = distance;
+			heighestRoomIndex = i;
+		}
+	}
+
+	//set one of the non-edge cells in the room to 
+	cells[rooms[heighestRoomIndex][0] * 2 + 2 + floor(random(0, rooms[heighestRoomIndex][2] * 2 - 1))][rooms[heighestRoomIndex][1] * 2 + 2 + floor(random(0, rooms[heighestRoomIndex][3] * 2 - 1))].changeColour(255, 0, 0);
+}
+
+function isDeadEnd(pos)
+{
+	let count = 0;
+	let dir = null;
+	for (let i = 0; i < 4 && count <= 1; i++)
+	{
+		if (!cells[pos[0] + VertexMazeCell.getDirectionX(i)][pos[1] + VertexMazeCell.getDirectionY(i)].walled)
+		{
+			dir = i;
+			count++;
+		}
+	}
+	return count == 1 ? dir : null;
+}
+
+function getValidPosition()
+{
+	let xStart = floor(random(0, vertexCols));
+	let yStart = floor(random(0, vertexRows));
+
+	let x = xStart;
+	let y = yStart;
+
+	do
+	{
+		do
+		{
+			if (cells[x * 2 + 1][y * 2 + 1].walled)
+			{
+				return [x * 2 + 1, y * 2 + 1];
+			}
+			y = (y + 1) % vertexRows;
+		} while(y != yStart)
+		x = (x + 1) % vertexCols;
+	} while(x != xStart)
+
+	return null;
+}
+
+function isInBounds(x, y)
+{
+	return x >= 0 && y >= 0 && x < cols - 1 / 2 && y < rows - 1 / 2;
+}
+
+function doMazeGen()
+{
+	let curIndex = activeCells.length - 1;
+
+	if (activeCells[curIndex].initializedEdgeCount === 4)
+	{
+		activeCells.pop();
+		return;
+	}
+
+	let dir = activeCells[curIndex].getAvailableDirection;
+	let x = activeCells[curIndex].x + VertexMazeCell.getDirectionX(dir) * 2;
+	let y = activeCells[curIndex].y + VertexMazeCell.getDirectionY(dir) * 2;
+
+	if (isInBounds(x,y) && cells[x][y].walled)
+	{
+		activeCells[curIndex].setPassage(dir, Passage.Open);
+		activeCells.push(cells[x][y]);
+		cells[x][y].awake(curRegion);
+	}
+	else
+	{
+		activeCells[curIndex].setPassage(dir, Passage.Blocked);
+	}
+}
+
+function getCell(x, y)
+{
+	if (x >= 0 && y >= 0 && x < cols && y < rows)
+	{
+		return cells[x][y];
+	}
+	return null;
+}
+
+function draw() 
+{
+	//draw background colour
+	background(0);
+	noStroke();
+	strokeWeight(0.5); 
+
+	for (let x = 0; x < cols; x++)
+	{
+		for (let y = 0; y < rows; y++)
+		{
+			cells[x][y].render();
+		}
+	}
+}
